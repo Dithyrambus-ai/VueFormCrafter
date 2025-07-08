f2c1ca922d81ea85c590d2b99274c89866c6b432
李昌昊 <18834162416@126.com>
2025-07-08 18:41:51 +0800

    style(HomeView): 格式化代码并移除多余空格
    统一代码格式，包括分号添加和空格调整，提升代码可读性

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
-import { Plus, Compass } from '@element-plus/icons-vue'
-import { ref } from 'vue'
+import { Plus, Compass } from '@element-plus/icons-vue';
+import { ref } from 'vue';
 
-const tableData = ref([])
-
+const tableData = ref([]);
 </script>
 
 <template>
   <div class="padding-20">
     <h1 class="font-weight-100 text-center">问卷系统</h1>
     <div class="mb-15">
-      <el-button type="primary"  :icon="Plus">创建问卷</el-button>
+      <el-button type="primary" :icon="Plus">创建问卷</el-button>
       <el-button type="success" :icon="Compass">组件市场</el-button>
     </div>
-    <el-table :data="tableData"  style="width: 100%;" border>
+    <el-table :data="tableData" style="width: 100%" border>
       <el-table-column label="创建日期" prop="createDate" width="150"></el-table-column>
       <el-table-column label="问卷标题" prop="title"></el-table-column>
-      <el-table-column label="题目数" prop="surveyCount" width="150" align="center"></el-table-column>
-      <el-table-column label="最近更新日期" prop="updateDate" width="150" align="center"></el-table-column>
+      <el-table-column
+        label="题目数"
+        prop="surveyCount"
+        width="150"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        label="最近更新日期"
+        prop="updateDate"
+        width="150"
+        align="center"
+      ></el-table-column>
       <el-table-column label="操作" width="300" align="center">
-        <template >
+        <template>
           <el-button type="primary" size="mini">查看问卷</el-button>
           <el-button type="primary" size="mini">编辑</el-button>
           <el-button type="danger" size="mini">删除</el-button>
@@ -29,5 +38,4 @@ const tableData = ref([])
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
